2b295e499afe99b528d818180addf8e4a2216c93
Timothy R. Batchelor <tbatch@mediawiz.com>
2021-01-27 12:07:18 -0500

    Adding eslint, prettier and test commands for the CI pipeline.

--- src/config/http.client.js
+++ src/config/http.client.js
@@ -1,31 +1,32 @@
-import axios from 'axios'
-import TokenGenerator from './token.generator'
+import axios from "axios";
+import TokenGenerator from "./token.generator";
 
 class HttpClient {
   initialize(baseUrl) {
-    if (this._axiosInstance)
-      throw new Error('HttpClient already initialized!');
+    if (this._axiosInstance) throw new Error("HttpClient already initialized!");
 
     this._axiosInstance = axios.create({
       baseURL: baseUrl,
-      timeout: 5000
+      timeout: 5000,
     });
 
-    this._axiosInstance.interceptors.request.use(async function tokenInterceptor(config) {
-      const accessToken = await TokenGenerator.get();
+    this._axiosInstance.interceptors.request.use(
+      async function tokenInterceptor(config) {
+        const accessToken = await TokenGenerator.get();
 
-      return {
-        ...config,
-        headers: {
-          ...config.headers,
-          Authorization: `Bearer ${accessToken}`
-        }
+        return {
+          ...config,
+          headers: {
+            ...config.headers,
+            Authorization: `Bearer ${accessToken}`,
+          },
+        };
       }
-    });
+    );
   }
 
   get instance() {
-    return this._axiosInstance
+    return this._axiosInstance;
   }
 }
 const httpClient = new HttpClient();
