e0aecc651953f0184b1776b63fb857f69b3db695
Timothy R. Batchelor <tbatch@mediawiz.com>
2021-02-08 23:58:43 -0500

    process.env is defined locally.

--- src/FilterableZoomAgentTable.js
+++ src/FilterableZoomAgentTable.js
@@ -152,18 +152,10 @@ const FilterableZoomAgentTable = (props) => {
   const [agents, setAgents] = useState([]);
   const [totalPages, setTotalPages] = useState(1);
   const [selectedAgentId, setSelectedAgentId] = useState();
-  const PAGE_QUERY_PARAM =
-    typeof process.env.REACT_APP_PAGE_QUERY_PARAM !== "undefined"
-      ? process.env.REACT_APP_PAGE_QUERY_PARAM
-      : "_page";
-  const LIMIT_QUERY_PARAM =
-    typeof process.env.REACT_APP_LIMIT_QUERY_PARAM !== "undefined"
-      ? process.env.REACT_APP_LIMIT_QUERY_PARAM
-      : "_limit";
-  const ENDPOINT_BASE_URL =
-    typeof process.env.REACT_APP_ENDPOINT_BASE !== "undefined"
-      ? process.env.REACT_APP_ENDPOINT_BASE
-      : "http://localhost:8000";
+
+  const PAGE_QUERY_PARAM = process.env.REACT_APP_PAGE_QUERY_PARAM;
+  const LIMIT_QUERY_PARAM = process.env.REACT_APP_LIMIT_QUERY_PARAM;
+  const ENDPOINT_BASE_URL = process.env.REACT_APP_ENDPOINT_BASE;
 
   const pageLength = 10;
 
